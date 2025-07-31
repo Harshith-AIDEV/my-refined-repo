3b5093a0fca083844b36161572eccdf551123067
harshith kottugumada <harshithkottugumada1977@gmail.com>
2025-07-31 13:17:55 +0530

    madlibs game file

---
+++ mad_libs_game.py
@@ -0,0 +1,31 @@
+
+print("welcome to the madlibs game! let us start the story...")
+noun1 =input("enter a boy's name:")
+adjective1 =input("enter an adjective(how passionately he watches anime):")
+adjective2 =input ("enter an adjective describing state of that boy emotion:")
+verb1 =input("enter a verb:")
+adjective3 =input("enter another adjective:")
+noun2 =input("enter a noun (something he wants ):")
+noun3 =input("enter a noun (group of people):")
+verb2 =input("enter a verb:")
+noun4 =input("enter a noun:")
+adjective4 =input("enter a adjective:")
+verb3 =input("enter a verb:")
+
+#The story begins
+print(f"In a village there lived a boy named {noun1}")
+print(f"and he used to {adjective1} watching anime ")
+print(f"he wanted to experience real japanese culture and he searched for it")
+print(f"He loved the culture of japan {adjective2} it is similar to his country,so he {verb1}to go to japan")
+print(f"he could go when he gets a job but he wanted to go as a student")
+print(f"Because as a student he can {adjective3} more")
+print(f"so he searched and found few universities to study there.")
+print(f"for him to get {noun2} in that university,he needs help from {noun3}")
+print(f"so he followed few students of that university to get guidance from them")
+
+
+
+print(f"He is {verb2} to get help from them and if they did help him this {noun4} will be {adjective4}")
+print(f"And his {verb3} might be fulfilled")
+print(f"Arigatou gozaimasu...")
+print(f"Thank you very much...")
